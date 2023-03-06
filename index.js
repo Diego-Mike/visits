e90f2b331bd0355c3c724fe6a2c766d08cf0dfc9
Diego C <usuario@MacBook-Pro-de-Usuario.local>
2023-03-06 16:32:13 -0500

    Damn

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import Fastify from "fastify";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const fastify = Fastify({ logger: true });
+
+const PORT = process.env.PORT;
 
 fastify.get("/", (req, rep) => {
   rep.send({ message: "healthy state" });
 });
 
-fastify.post("/", async (req, rep) => {
+fastify.post("/visit", async (req, rep) => {
   return {
     status: "successful",
     error: false,
@@ -18,7 +23,8 @@ fastify.post("/", async (req, rep) => {
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port: PORT });
+    console.log(`Server ready on port ${PORT}`);
   } catch (error) {
     fastify.log.error(error);
     process.exit(1);
